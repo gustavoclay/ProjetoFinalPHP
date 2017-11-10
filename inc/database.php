011d62d4b8d8bde7374b32c5c1c686a9a0df0ce7
Gustavo Clay <gustavoclay@gmail.com>
2017-11-10 18:05:46 -0200

    Criação do arquivo database.php como fabrica de conexões ao bando de dados da aplicação

---
+++ inc/database.php
@@ -0,0 +1,25 @@
+<?php
+
+/*Este arquivo possui as funções para fabricar conexões com o banco de dados*/
+
+mysqli_report(MYSQL_REPORT_STRICT);
+
+function open_database() {
+	try {
+		$conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+		return $conn;
+	} catch (Exception $e) {
+		echo $r->getMessage();
+		return null;
+	}
+}
+
+function close_database() {
+	try {
+		mysqli_close($conn);
+	} catch (Exception $e) {
+		echo $e->getMessag();
+	}
+}
+
+?>
